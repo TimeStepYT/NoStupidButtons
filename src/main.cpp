29b84b8a7e97d5d15f709c640eb47f9dd0382f63
Reinmmar <herwig.orlich@gmail.com>
2024-06-27 14:31:56 +0200

    hide social media buttons and optionally robtop logo

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,60 @@ using namespace geode::prelude;
 
 #include <Geode/modify/MenuLayer.hpp>
 
+bool isSettingEnabled(std::string setting) {
+    return Mod::get()->getSettingValue<bool>(setting);
+}
+
 class $modify(MyMenuLayer, MenuLayer) {
+    struct Fields {
+        CCNode *bottomMenu;
+        CCNode *socialMediaMenu;
+        CCNode *moreGamesMenu;
+    };
+
     bool init() {
         if (!MenuLayer::init()) {
             return false;
-				}
+        }
 
-				if (Mod::get()->getSettingValue<bool>("hide-newgrounds-button")) {
-						auto bottomMenu = this->getChildByID("bottom-menu");
-						bottomMenu->removeChildByID("newgrounds-button");
-						bottomMenu->updateLayout();
-				}
+        m_fields->bottomMenu = this->getChildByID("bottom-menu");
+        m_fields->socialMediaMenu = this->getChildByID("social-media-menu");
+        m_fields->moreGamesMenu = this->getChildByID("more-games-menu");
+
+        if (isSettingEnabled("hide-newgrounds-button")) {
+            m_fields->bottomMenu->removeChildByID("newgrounds-button");
+            m_fields->bottomMenu->updateLayout();
+        }
+
+        if (isSettingEnabled("hide-social-media-buttons")) {
+            m_fields->socialMediaMenu->removeChildByID("facebook-button");
+            m_fields->socialMediaMenu->removeChildByID("twitter-button");
+            m_fields->socialMediaMenu->removeChildByID("youtube-button");
+            m_fields->socialMediaMenu->removeChildByID("twitch-button");
+            m_fields->socialMediaMenu->removeChildByID("discord-button");
+            m_fields->socialMediaMenu->updateLayout();
+        }
+
+        if (isSettingEnabled("hide-robtop-logo")) {
+            m_fields->socialMediaMenu->removeChildByID("robtop-logo-button");
+            m_fields->socialMediaMenu->updateLayout();
+        } else if (isSettingEnabled("hide-social-media-buttons")) {
+            CCNode *robtopButtonNode =
+                m_fields->socialMediaMenu->getChildByID("robtop-logo-button");
+            CCNode *bottomMenuButton = m_fields->bottomMenu->getChildByID("settings-button");
+            robtopButtonNode->setPositionY(bottomMenuButton->getPositionY());
+            m_fields->socialMediaMenu->updateLayout();
+        }
 
 #ifdef GEODE_IS_ANDROID
-				if (Loader::get()->isModLoaded("geode.devtools"))
-						return true;
+        if (Loader::get()->isModLoaded("geode.devtools"))
+            return true;
 #endif
-				if (Mod::get()->getSettingValue<bool>("hide-more-games-button")) {
-						auto moreGamesMenu = this->getChildByID("more-games-menu");
-						moreGamesMenu->removeChildByID("more-games-button");
-						moreGamesMenu->updateLayout();
-				}	
+        if (isSettingEnabled("hide-more-games-button")) {
+            m_fields->moreGamesMenu->removeChildByID("more-games-button");
+            m_fields->moreGamesMenu->updateLayout();
+        }
 
-				return true;
-		}
+        return true;
+    }
 };
